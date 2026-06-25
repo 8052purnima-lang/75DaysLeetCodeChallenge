a539aef7aff4196dec7ab5a3d52b846e70d22568
Purnima_Singh8052 <8052purnima@gmail.com>
2026-06-25 11:25:46 +0530

    Time: 0 ms (100%), Space: 20.7 MB (36.88%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+
+    int jump(vector<int>& nums) {
+        int maxi = 0, curr =0, count=0;
+        for(int i=0; i<nums.size()-1; i++){
+            maxi = max(maxi, nums[i]+i);
+            if(i == curr){
+                count++;
+                curr = maxi;
+            }
+        }
+        return count;
+
+    }
+};
